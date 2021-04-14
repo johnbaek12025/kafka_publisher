3ad4cbc117a234e9406556327ff5756bf376efd2
cyg <tp.rc.cyg@gmail.com>
2021-04-14 14:14:11 +0900

    temp

--- kpm/pubs/admin_adapter.py
+++ kpm/pubs/admin_adapter.py
@@ -1,10 +1,9 @@
 import json
 import logging
+import socket
 import time
 
 from kpm.kafka_manager import KafkaPub
-from kpm.publisher_manager import PublisherManager
-
 
 logger = logging.getLogger(__name__)
 
@@ -19,39 +18,49 @@ class Publisher(object):
 
     def run(self):
         self.connect_to_broker()
+        try:
+            while not self.stop_requested:
+                rows = self.get_data_to_handle()
+                for row in rows:
+                    news_code = row.get("news_code")
+                    news_sn = row.get("news_sn")
+                    d_news_crt = row.get("d_news_crt")
+                    topic = row.get("topic")
 
-        while not self.stop_requested:
-            rows = self.get_data_to_handle()
-            for row in rows:
-                news_code = row.get("news_code")
-                news_sn = row.get("news_sn")
-                d_news_crt = row.get("d_news_crt")
-                topic = row.get("topic")
-
-                # broker 에 전송여부 상관없이, 읽어온 데이터는 무조서 성공처리
-                self.change_requests_status(
-                    sn=news_sn, d_crt=d_news_crt, news_code=news_code
-                )
-
-                if None in [news_code, news_sn, d_news_crt]:
-                    logger.info(
-                        f"Please check message: news_code: {news_code}, news_sn: {news_sn}, d_news_crt: {d_news_crt}"
+                    # broker 에 전송여부 상관없이, 읽어온 데이터는 무조서 성공처리
+                    self.change_requests_status(
+                        sn=news_sn, d_crt=d_news_crt, news_code=news_code
                     )
-                    continue
 
-                message = {
-                    "news_code": news_code,
-                    "news_sn": news_sn,
-                    "d_news_crt": d_news_crt,
-                }
+                    if None in [news_code, news_sn, d_news_crt]:
+                        logger.info(
+                            f"Please check message: news_code: {news_code}, news_sn: {news_sn}, d_news_crt: {d_news_crt}"
+                        )
+                        continue
 
-                self.kafka_mgr.send_messages([(topic, message, 0.01)])
-            time.sleep(1)
+                    message = {
+                        "news_code": news_code,
+                        "news_sn": news_sn,
+                        "d_news_crt": d_news_crt,
+                    }
+
+                    self.kafka_mgr.send_messages([(topic, message, 0.01)])
+                time.sleep(10)
+        except (socket.error, socket.timeout, socket.herror) as e:
+            logger.error(f"socket failure: {e}")
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.disconnect_from_broker()
 
     def connect_to_broker(self):
         logger.info("connect to kafka")
         self.kafka_mgr = KafkaPub()
         self.kafka_mgr.connect(**self.kafka_info)
+
+    def disconnect_from_broker(self):
+        if self.kafka_mgr:
+            self.kafka_mgr.disconnect()
 
     def change_requests_status(self, sn, d_crt, news_code, status="S", commit=True):
         sql = f"""
